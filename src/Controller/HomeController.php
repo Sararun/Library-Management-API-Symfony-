60508eef9f553815981ad0ca3602fa06d4fb0383
sararun <sarosekvlad12@gmail.com>
2025-08-25 17:16:06 +0700

    Накрутил свою докер конфигурацию

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to Symfony!',
+            'version' => \Symfony\Component\HttpKernel\Kernel::VERSION,
+            'environment' => $this->getParameter('kernel.environment'),
+            'debug' => $this->getParameter('kernel.debug'),
+        ]);    }
+}
