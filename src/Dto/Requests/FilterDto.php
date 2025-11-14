b6336827635c1da6eeae9e4fbdd4e03d086c698e
sararun <sarosekvlad12@gmail.com>
2025-11-14 18:07:04 +0700

    Фикс для нулевых значений

--- src/Dto/Requests/FilterDto.php
+++ src/Dto/Requests/FilterDto.php
@@ -6,13 +6,16 @@ use Symfony\Component\Validator\Constraints as Assert;
 class FilterDto
 {
     #[Assert\NotBlank]
+    #[Assert\NotNull]
     #[Assert\Type('string')]
     public string $field;
 
     #[Assert\NotBlank]
+    #[Assert\NotNull]
     #[Assert\Type('string')]
     public string $type;
 
     #[Assert\NotNull]
+    #[Assert\NotBlank]
     public mixed $value;
 }
